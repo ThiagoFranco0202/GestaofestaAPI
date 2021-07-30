0a2d1ccab3ad5fa13292398cac668b8f3721b359
ThiagoFranco0202 <46629705+ThiagoFranco0202@users.noreply.github.com>
2021-07-30 13:15:30 -0300

    Add files via upload
    script sql comentado

--- src/main/resources/postgres/insert_for_Test.sql
+++ src/main/resources/postgres/insert_for_Test.sql
@@ -1,2 +1,3 @@
+--Dados para inserir no banco de dados para conseguir rodar os testes com junit
 delete from public.festa where id = 500;
 insert into public.festa values(500,false,'convidadoTeste',2);
