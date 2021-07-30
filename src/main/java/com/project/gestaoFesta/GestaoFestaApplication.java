f297f8f08239c1c58736d45ff4be37ee226ec849
ThiagoFranco0202 <46629705+ThiagoFranco0202@users.noreply.github.com>
2021-07-30 13:00:46 -0300

    Add files via upload
    Main comentado

--- src/main/java/com/project/gestaoFesta/GestaoFestaApplication.java
+++ src/main/java/com/project/gestaoFesta/GestaoFestaApplication.java
@@ -13,11 +13,12 @@ public class GestaoFestaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GestaoFestaApplication.class, args);
 	}
-
+	
+	//Adicionando o Swagger na aplicação, foi colocado o apache 2.0 como meio de demonstração apenas
 	@Bean
 	public OpenAPI customOpenAPI(){
 		return new OpenAPI().info(new Info()
-				.title("")
+				.title("Gestão Festa API")
 				.version("1.0")
 				.termsOfService("https://swagger.io/terms")
 				.license(new License().name("Apache 2.0").url("http://springdoc.org")))	;
