c6e2278c34c87accecfd44d4bcf29d7ed1e13c31
ThiagoFranco0202 <46629705+ThiagoFranco0202@users.noreply.github.com>
2021-07-30 12:58:18 -0300

    Add files via upload
    Service comentado

--- src/main/java/com/project/gestaoFesta/service/ConvidadosService.java
+++ src/main/java/com/project/gestaoFesta/service/ConvidadosService.java
@@ -8,39 +8,42 @@ import com.project.gestaoFesta.repository.Convidados;
 import java.util.List;
 import java.util.Optional;
 
+//Camada de serviço para ligar o controller com o repository
 @Service
 public class ConvidadosService {
 
     @Autowired
     private Convidados repository;
 
-    //testado
+    //método responsável por salvar um convidado
     public Convidado save(Convidado convidado) {
         repository.save(convidado);
         return convidado;
     }
 
-    //testado
+    //método responsável por remover um convidado
     public Optional<Convidado> delete(Long id) {
         Optional<Convidado> convidado = this.findById(id);
         repository.deleteById(id);
         return convidado;
     }
 
-    //testado
+    //Busca e Retorna um convidado pelo seu Id
     public Optional<Convidado> findById(Long id) {
         return repository.findById(id);
     }
 
+    //Busca e retorna uma lista com todos os convidados
     public List<Convidado> findAll(){
         List<Convidado> list = repository.findAll();
         return list;
     }
 
+    //Confirma a presença de um convidado
     public Convidado checkPresence(Long id_convidado) {
-        Optional<Convidado> aux = this.repository.findById(id_convidado);
+        Optional<Convidado> aux = this.repository.findById(id_convidado);//Busca o convidado pelo Id
 
-        aux.get().setConfirmado(true);
+        aux.get().setConfirmado(true);//Muda seu status de presença para true
 
         return this.repository.saveAndFlush(aux.get());
     }
